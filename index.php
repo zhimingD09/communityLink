fb9d83fdd1c146b4bce47ad6e4f26ff4729b9d32
Your Name <you@example.com>
2025-09-05 12:34:00 +1000

    Fix routing system to handle dynamic routes with parameters

--- index.php
+++ index.php
@@ -8,56 +8,80 @@ $request = $_SERVER['REQUEST_URI'];
 $basePath = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
 $request = str_replace($basePath, '', $request);
 
+// Parse URL
+$url = parse_url($request, PHP_URL_PATH);
+$url = trim($url, '/');
+$urlParts = explode('/', $url);
+
 // Route the request
-switch ($request) {
-    case '/':
-    case '':
-        require __DIR__ . '/controllers/Pages.php';
-        $pages = new Pages();
-        $pages->index();
-        break;
-    case '/about':
+if (empty($url)) {
+    // Home page
+    require __DIR__ . '/controllers/Pages.php';
+    $pages = new Pages();
+    $pages->index();
+} else {
+    // Get controller
+    $controller = $urlParts[0];
+    
+    // Get method (if exists)
+    $method = isset($urlParts[1]) ? $urlParts[1] : 'index';
+    
+    // Get parameters (if exists)
+    $params = array_slice($urlParts, 2);
+    
+    // Map specific routes
+    if ($controller == 'about') {
         require __DIR__ . '/controllers/Pages.php';
         $pages = new Pages();
         $pages->about();
-        break;
-    case '/volunteers/register':
+    } elseif ($controller == 'volunteers' && $method == 'register') {
         require __DIR__ . '/controllers/Users.php';
         $users = new Users();
         $users->registerVolunteer();
-        break;
-    case '/organizations/register':
+    } elseif ($controller == 'organizations' && $method == 'register') {
         require __DIR__ . '/controllers/Users.php';
         $users = new Users();
         $users->registerOrganization();
-        break;
-    case '/login':
+    } elseif ($controller == 'login') {
         require __DIR__ . '/controllers/Users.php';
         $users = new Users();
         $users->login();
-        break;
-    case '/logout':
+    } elseif ($controller == 'logout') {
         require __DIR__ . '/controllers/Users.php';
         $users = new Users();
         $users->logout();
-        break;
-    case '/admin':
+    } elseif ($controller == 'admin') {
         require __DIR__ . '/controllers/Admin.php';
         $admin = new Admin();
         $admin->index();
-        break;
-    case '/events':
+    } elseif ($controller == 'events') {
         require __DIR__ . '/controllers/Events.php';
         $events = new Events();
-        $events->index();
-        break;
-    case '/profile':
+        
+        if ($method == 'index') {
+            $events->index();
+        } elseif ($method == 'show' && !empty($params[0])) {
+            $events->show($params[0]);
+        } elseif ($method == 'create') {
+            $events->create();
+        } elseif ($method == 'edit' && !empty($params[0])) {
+            $events->edit($params[0]);
+        } elseif ($method == 'delete' && !empty($params[0])) {
+            $events->delete($params[0]);
+        } elseif ($method == 'register' && !empty($params[0])) {
+            $events->register($params[0]);
+        } elseif ($method == 'unregister' && !empty($params[0])) {
+            $events->unregister($params[0]);
+        } else {
+            http_response_code(404);
+            require __DIR__ . '/views/pages/404.php';
+        }
+    } elseif ($controller == 'profile') {
         require __DIR__ . '/controllers/Users.php';
         $users = new Users();
         $users->profile();
-        break;
-    default:
+    } else {
         http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
+        require __DIR__ . '/views/pages/404.php';
+    }
 }
